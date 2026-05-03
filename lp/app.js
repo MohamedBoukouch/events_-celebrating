fecc5a63f0563463c6c5e6b42565e3cd3b4970e6
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2026-05-04 00:54:30 +0100

    errors fixed

--- lp/app.js
+++ lp/app.js
@@ -509,7 +509,7 @@ async function uploadOneImage(imgObj) {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      action: 'uploadImage',
+      action: 'uploadRequestImage',
       data: imgObj.base64,
       mimeType: imgObj.mime,
       filename: imgObj.name
